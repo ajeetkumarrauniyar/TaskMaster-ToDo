a34665526dc91b88836e9a8cc881c5f52cf8c068
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2023-11-03 11:32:22 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -112,8 +112,9 @@ function AddTask() {
             className="block w-full sm:w-auto sm:mx-3 rounded-md border-0 px-3.5 py-2.5 sm:py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:text-sm focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             value={taskInput.task}
             onChange={
-              (e) => setTaskInput({ ...setTaskInput, task: e.target.value })
-              // setTaskInput({ task: e.target.value })
+              (e) => 
+              // setTaskInput({ ...setTaskInput, task: e.target.value })
+              setTaskInput({ task: e.target.value })
             }
           />
 
